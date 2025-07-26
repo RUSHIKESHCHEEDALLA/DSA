23aa5e5b5ccaf8ecfd752a65f298a4c3aab996e0
RUSHIKESHCHEEDALLA <139362278+RUSHIKESHCHEEDALLA@users.noreply.github.com>
2025-07-26 17:02:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (54.25%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -21,16 +21,32 @@ class Solution(object):
         #     dp[ind]=max(p,np)    
         #     return dp[ind]
         # return fa(len(nums)-1)
-        if len(nums)<=2:
-            return max(nums)
-        dp=[0 for _ in range(len(nums))]
-        dp[0]=nums[0]
-        dp[1]=max(nums[0],nums[1])
-        print(dp)
-        for i in range(2,len(nums)):
-            dp[i]=nums[i]+max(dp[0:i-1])
-        print(dp)
-        return max(dp[-1],dp[-2])
+        # if len(nums)<=2:
+        #     return max(nums)
+        # dp=[0 for _ in range(len(nums))]
+        # dp[0]=nums[0]
+        # dp[1]=max(nums[0],nums[1])
+        # print(dp)
+        # for i in range(2,len(nums)):
+        #     dp[i]=nums[i]+max(dp[0:i-1])
+        # print(dp)
+        # return max(dp[-1],dp[-2])
+
+
+        if len(nums)==1:
+            return nums[0]
+        p2=0
+        p1=nums[0]
+        for i in range(1,len(nums)):
+            t=nums[i]
+            if i>1:
+                t+=p2
+            nt=p1
+            c=max(t,nt)
+            p2=p1
+            p1=c
+        return p1
+        
 
             
 
